3e3ba81db81446c56a2e6940648d107c1a1079f3
Marco Centonze <135604317+marcocentonze@users.noreply.github.com>
2024-03-13 15:43:14 +0100

    Add files via upload

---
+++ ResocontoDigitale/DispositivoElettronico.java
@@ -0,0 +1,6 @@
+public class DispositivoElettronico {
+    public String avviaApplicazione(String nameApp) {
+        System.out.println("L'applicazione si sta avviando e si chiama" + nameApp);
+        return nameApp;
+    }
+}

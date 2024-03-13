413352ab3d5264f3b52c6cfe3b68cfbe27e677f9
Marco Centonze <135604317+marcocentonze@users.noreply.github.com>
2024-03-13 16:09:23 +0100

    Add files via upload
    add Smartphone class

--- ResocontoDigitale/DispositivoElettronico.java
+++ ResocontoDigitale/DispositivoElettronico.java
@@ -1,6 +1,46 @@
+
+import java.util.Scanner;
+
 public class DispositivoElettronico {
-    public String avviaApplicazione(String nameApp) {
+    public void avviaApplicazione(String nameApp) {
         System.out.println("L'applicazione si sta avviando e si chiama" + nameApp);
-        return nameApp;
     }
+
+    public class Smartphone extends DispositivoElettronico {
+        private String nome;
+        private String password;
+        Scanner scanner = new Scanner(System.in);// inizializzo il mi scanner
+
+        public Smartphone(String nome, String password) {
+            this.nome = nome;
+            this.password = password;
+        }
+
+        // faccio l'override
+        public void avviaApplicazione(String nomeApp) {
+            System.out.println("Si sta avviando" + nomeApp
+                    + "su smartphone");
+            loggin(); // richiamo la funzione
+
+        }
+
+        // mi creo la funzione per il login
+        private void loggin() {
+            String nomeValido = "Marco";
+            String passwordAccettata = "Centonze";
+            System.out.print("Inserisci il tuo nome: ");
+            this.nome = scanner.nextLine();
+
+            System.out.print("Inserisci la tua password: ");
+            this.password = scanner.nextLine();
+
+            if (nome == nomeValido && password == passwordAccettata) {
+                System.out.println("Hai effettuato correttamente l'accesso");
+            } else {
+                return; // da testare
+            }
+
+        }
+    }
+
 }
